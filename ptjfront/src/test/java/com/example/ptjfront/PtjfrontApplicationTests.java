2a22942d2f776831b62c61a63c8e8e1b1cc8f0ce
NORTH205-19\북205_20 <knkn9436@naver.com>
2023-11-06 17:52:34 +0900

    프로젝트 프론트 레이아웃 구성 (메인, 회원가입, 로그인, 상품상세)

---
+++ ptjfront/src/test/java/com/example/ptjfront/PtjfrontApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ptjfront;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PtjfrontApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
